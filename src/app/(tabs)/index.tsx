70dcadfc9109ddef45c931b0eb4d23f1fc93cdc8
Baiquan Fan <baiquan.fan@gmail.com>
2024-07-12 15:40:03 +0300

    Image Rendering and Styling.

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,15 +1,16 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
 import Colors from '@/src/constants/Colors';
 import products from '../../../assets/data/products'
 
 // import EditScreenInfo from '@/src/components/EditScreenInfo';
 // import { Text, View } from '@/src/components/Themed';
 
-const product = products[0];
+const product = products[1];
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
+      <Image source={{ uri: product.image }} style={styles.image} />
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
     </View>
@@ -18,6 +19,13 @@ export default function TabOneScreen() {
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 20,
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 1,
   },
   title: {
     fontSize: 18,

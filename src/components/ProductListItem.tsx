838a543521c001e78f4305dd0d411efbd2974585
Baiquan Fan <baiquan.fan@gmail.com>
2024-07-12 17:12:42 +0300

    Fixing UI Issues.

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,11 +1,18 @@
 import { StyleSheet, View, Text, Image } from 'react-native';
 import Colors from '@/src/constants/Colors';
+import { Product } from '../types';
 
-const ProductListItem = ({ product }) => {
-  console.log(product);
+export const defaultPizzaImage =
+    'http://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
+
+type ProductListItemProps = {
+    product: Product;
+}
+
+const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
     <View style={styles.container}>
-      <Image source={{ uri: product.image }} style={styles.image} />
+      <Image source={{ uri: product.image || defaultPizzaImage }} style={styles.image} />
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
     </View>

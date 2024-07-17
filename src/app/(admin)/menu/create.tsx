05eed61b04197b9f42789705ec203e5bbfe358a7
Baiquan Fan <baiquan.fan@gmail.com>
2024-07-17 17:37:03 +0300

    Image Picker Implementation.

--- src/app/(admin)/menu/create.tsx
+++ src/app/(admin)/menu/create.tsx
@@ -1,11 +1,15 @@
-import { View, Text, StyleSheet, TextInput } from 'react-native'
+import { View, Text, StyleSheet, TextInput, Image } from 'react-native'
 import React, { useState } from 'react'
 import Button from '@/components/Button';
+import { defaultPizzaImage } from '@/components/ProductListItem';
+import Colors from '@/constants/Colors';
+import * as ImagePicker from 'expo-image-picker';
 
 const CreateProductScreen = () => {
     const [name, setName] = useState('');
     const [price, setPrice] = useState('');
     const [errors, setErrors] = useState('');
+    const [image, setImage] = useState<string | null>(null);
 
     const resetFields = () => {
         setName('');
@@ -44,8 +48,24 @@ const CreateProductScreen = () => {
         resetFields();
     }
 
+    const pickImage = async () => {
+        // No permissions request is necessary for launching the image library
+        let result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.Images,
+          allowsEditing: true,
+          aspect: [4, 3],
+          quality: 1,
+        });
+    
+        if (!result.canceled) {
+          setImage(result.assets[0].uri);
+        }
+      };
+
   return (
     <View style={styles.container}>
+      <Image source={{ uri: image || defaultPizzaImage }} style={styles.image} />
+      <Text style={styles.textButton} onPress={pickImage}>Select image</Text>
       <Text style={styles.label}>create</Text>
       <TextInput
         placeholder="name"
@@ -72,6 +92,17 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         padding: 10
+    },
+    image: {
+        width: '50%',
+        aspectRatio: 1,
+        alignSelf: 'center'
+    },
+    textButton: {
+        alignSelf: 'center',
+        fontWeight: 'bold',
+        color: Colors.light.tint,
+        marginVertical: 10,
     },
     label: {
         color: 'gray',

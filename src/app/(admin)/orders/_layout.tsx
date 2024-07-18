e3228829ed3330f7f23267adf9f5251019f70168
Baiquan Fan <baiquan.fan@gmail.com>
2024-07-18 21:44:12 +0300

    Orders Top Tab Navigator.

--- src/app/(admin)/orders/_layout.tsx
+++ src/app/(admin)/orders/_layout.tsx
@@ -2,8 +2,9 @@ import { Stack } from 'expo-router';
 
 export default function MenuStack() {
     return (
-        <Stack >
-            <Stack.Screen name="index" options={{ title: 'Orders'}} />
+        <Stack>
+            {/* <Stack.Screen name="index" options={{ title: 'Orders' }} /> */}
+            <Stack.Screen name="list" options={{ headerShown: false }} />
         </Stack>
     );
 }
